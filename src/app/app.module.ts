60cf11dd483668e3e39a9a643c12beac5b34ec46
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-08-11 08:23:06 -0400

    created a form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote.details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    StrikethroughDirective,
+    GoalFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+
+  constructor(private elem: ElementRef) { }
+
+  @HostListener(' click') onClick() {
+  this.textDeco(' underline overline');
+}
+
+  @HostListener(' dblclick') onDoubleClicks() {
+  this.textDeco(' None');
+  }
+  private textDeco(action: string) {
+    this.elem.nativeElement.style.textDecoration = action;
+  }
+  }

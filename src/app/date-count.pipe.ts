610c57b10c4008af55ab11bd7117c045707a0452
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-08-12 11:49:34 -0400

    date count working

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
+@Pipe({ // pipe decorator
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any, ): number {
+    let today: Date = new Date(); // this gets the current date and time
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference = Math.abs (value - todayWithNoTime ); // returns value in milliseconds
+    const secondsInADay = 86400; // 60 seconds*60 minutes in an hour *24 hours
+    var dateDifferenceSeconds = dateDifference * 0.01;
+    var dateCounter = dateDifferenceSeconds / secondsInADay;
+
+    if  (dateCounter >= 1 ) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
   }
 
 }
